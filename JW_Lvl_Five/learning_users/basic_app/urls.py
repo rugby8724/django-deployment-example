af8bca17c13fa2a3288fa0c37ed9e99325c9cd32
Jeremiah Wise <jeremiah.d.wise@gmail.com>
2019-01-07 14:01:59 -0600

    first

---
+++ JW_Lvl_Five/learning_users/basic_app/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from basic_app import views
+from django.conf.urls import url
+
+# Template URLs
+
+app_name = 'basic_app'
+
+urlpatterns=[
+    path('register/', views.register, name='register'),
+    path('user_login/', views.user_login, name='user_login' )
+
+]
